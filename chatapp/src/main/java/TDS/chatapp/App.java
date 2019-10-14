27e5897eddfec218f698c3a732bd031c5b400628
Jesús <jesusmalcaraz@gmail.com>
2019-10-14 16:01:16 +0200

    Start

---
+++ chatapp/src/main/java/TDS/chatapp/App.java
@@ -0,0 +1,13 @@
+package TDS.chatapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
