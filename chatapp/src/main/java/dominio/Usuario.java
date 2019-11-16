7e7281b66cd193b331ce17e47cb2fdc15c2e37ec
Arturo Lorenzo <smitexx@users.noreply.github.com>
2019-11-16 16:17:30 +0100

    añadir clases del dominio

--- chatapp/src/main/java/dominio/Usuario.java
+++ chatapp/src/main/java/dominio/Usuario.java
@@ -1,5 +1,62 @@
 package dominio;
 
-public class Usuario {
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Usuario {
+	private int codigo;
+	private String email;
+	private String nombre;
+	private String contraseña;
+	private String telefono;
+	private Date fechaNacimiento;
+	private List<Contacto> contactos;
+	private boolean premium;
+	private String rutaImg;
+	
+	public Usuario(int codigo, String email, String nombre, String contraseña, String telefono, Date fecha) {
+		this.codigo = codigo;
+		this.email = email;
+		this.nombre = nombre;
+		this.contraseña = contraseña;
+		this.telefono = telefono;
+		this.fechaNacimiento = fecha;
+		this.contactos = new LinkedList<Contacto>();
+		this.premium = false;
+		this.rutaImg = null;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public String getRutaImg() {
+		return rutaImg;
+	}
+	public List<Contacto> getContactos() {
+		return new LinkedList<Contacto>(this.contactos);
+	}
+	public void setRutaImg(String rutaImg) {
+		this.rutaImg = rutaImg;
+	}
+	public boolean isPremium() {
+		return this.premium;
+	}
+	public void setPremium(boolean premium) {
+		this.premium = premium;
+	}
 }
